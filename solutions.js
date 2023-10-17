e83386a5b90e0a74833c76dc31e2970e1719b8c0
Gabriel Urbano <gurbano1029@gmail.com>
2023-10-17 13:51:23 -0500

    completed Ten Functions

--- solutions.js
+++ solutions.js
@@ -21,3 +21,74 @@
  * concat
  */
 
+function isTrue(input) {
+    return input === true;
+}
+
+
+function isFalse(input) {
+    return input === false;
+}
+
+
+function not(input) {
+    return !input;
+}
+
+
+function addOne(input) {
+    if (typeof input === 'number' || (!isNaN(input) && !isNaN(parseFloat(input)))) {
+        return parseFloat(input) + 1;
+    } else {
+        return NaN;
+    }
+}
+
+
+function isEven(input) {
+    if (input % 2 === 0) {
+        return true;
+    } else (input % 2 === 1);
+    return false
+}
+
+
+function isIdentical(input, input1) {
+    return input === input1
+
+}
+
+
+function isEqual(input, input1) {
+    if (input == input1)
+        return true
+    else {
+        return false
+    }
+
+}
+
+
+function or(input, input1) {
+    return input || input1
+
+}
+
+
+function and(input, input1) {
+    return input && input1
+
+}
+
+
+function concat(input1, input2) {
+    if (typeof input1 === 'string' && typeof input2 === 'string') {
+        return input1 + input2
+    } else if (typeof input1 === 'number' && typeof input2 === 'number') {
+        return input1.toString() + input2.toString()
+    } else {
+        return String(input1) + String(input2)
+    }
+}
+
+// console.log(concat(true, true))
